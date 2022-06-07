5d218a29350bb417671024a5b60a9534f7e06635
Сергей Владиславович Голиков <igo077@ISTRA_INFO>
2022-06-07 16:24:55 +0300

    initial commit

---
+++ Dz6/Program.cs
@@ -0,0 +1,29 @@
+﻿int GetNumber(string message)
+{
+    Console.Write(message);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out int a)) 
+            if(a>0)
+                return a;
+            else
+                System.Console.WriteLine("ошибка 002 ! Введите не отрицательное число.");
+        else
+            System.Console.WriteLine("ошибка 001 ! Введите положительное число.");
+    }
+}
+
+int Rekurs(int m, int n)
+{
+    if (m == 0)
+        n++;
+    else
+        n = (m > 0 && n == 0) ? Rekurs(m - 1, 1):Rekurs(m - 1, Rekurs(m, n - 1));
+    return n;
+}
+
+Console.Clear();
+int m = GetNumber("Введите чило М = ");
+int n = GetNumber("Введите чило N = ");
+int result = Rekurs(m, n);
+Console.WriteLine($"{result} результат");
---
+++ Dz7/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int Summ(int m, int n)
+{
+    return m += (m < n) ? Summ(++m, n) : 0;
+}
+
+Console.Clear();
+System.Console.WriteLine("Сумма натуральных чисел в промежутке от M до N");
+System.Console.Write("Введите число M = ");
+int numM = int.Parse(Console.ReadLine());
+System.Console.Write("Введите число N = ");
+int numN = int.Parse(Console.ReadLine());
+int summ = Summ(numM, numN);
+Console.WriteLine($"M = {numM}; N = {numN}; ->  {summ} сумма");
